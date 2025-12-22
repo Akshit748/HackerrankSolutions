e5156ea6e63dee333cd4a09c5f26ad85a0d5e0b1
Akshit748 <73868401+Akshit748@users.noreply.github.com>
2025-12-22 21:27:26 +0530

    Create Exceptions.py

---
+++ Python/Exceptions.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for _ in range(n):
+    a,b = input().split()
+
+    try:
+        a,b = int(a),int(b)
+        print(a//b)
+
+    except Exception as e:
+        print("Error Code:",e)

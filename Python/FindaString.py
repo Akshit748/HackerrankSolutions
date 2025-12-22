d50d3dce6ca79e88b1b1963f22ca1681b022a3cf
Akshit748 <73868401+Akshit748@users.noreply.github.com>
2025-12-22 21:22:13 +0530

    Add function to count occurrences of a substring

---
+++ Python/FindaString.py
@@ -0,0 +1,6 @@
+def count_substring(string, sub_string,count=0):
+    for i in range(len(string)):
+        if string.find(sub_string,i,i+len(sub_string))!=-1:
+            count+=1
+    return count
+    
